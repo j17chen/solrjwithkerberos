78c50da49bdb88a5d1d3df306e5bca8c0d7a1730
jchen <jchen@hortonworks.com>
2018-11-07 02:30:50 -0800

    change for customer env

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,24 +11,24 @@ import org.apache.solr.common.SolrInputDocument;
  */
 public class Main {
     public static void main(String[] args) {
-        final String COLLECTION_NAME="ranger_audits";
+        final String COLLECTION_NAME="GQRS";
 
         try {
 
-            String zkHostString = "node1.openstacklocal.com:2181,node2.openstacklocal.com:2181,node3.openstacklocal.com:2181/solr";
+            String zkHostString = "hpcc02r01n01.hpc.ford.com:2181,hpcc02r01n03.hpc.ford.com:2181,hpcc02r01n06.hpc.ford.com:2181/solr";
             HttpClientUtil.addConfigurer(new MyKrb5HttpClientConfigurer());
             SolrClient solr = new CloudSolrClient.Builder().withZkHost(zkHostString).build();
             String id = String.valueOf(System.currentTimeMillis());
             //test write data
-            {
-                SolrInputDocument document = new SolrInputDocument();
-                document.addField("id", id);
-                document.addField("name", "Gouda cheese wheel");
-                document.addField("price", "49.99");
-                UpdateResponse response = solr.add(COLLECTION_NAME,document);
-                System.out.println(response.toString());
-                solr.commit(COLLECTION_NAME);
-            }
+//            {
+//                SolrInputDocument document = new SolrInputDocument();
+//                document.addField("id", id);
+//                document.addField("name", "Gouda cheese wheel");
+//                document.addField("price", "49.99");
+//                UpdateResponse response = solr.add(COLLECTION_NAME,document);
+//                System.out.println(response.toString());
+//                solr.commit(COLLECTION_NAME);
+//            }
             //test query data
             {
                 SolrQuery query = new SolrQuery();
